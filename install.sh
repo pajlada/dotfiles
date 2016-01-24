4508d4d351d1d2af71df08d66dda6916a2ecf42b
pajlada <pajlada@bithack.se>
2016-01-24 01:08:19 +0100

    more modifications

--- install.sh
+++ install.sh
@@ -7,11 +7,18 @@ for dotfile in ${dotfiles[*]}; do
     ln -s `pwd`/$dotfile ~/$dotfile 2>/dev/null
 done
 
-mkdir -p ~/.vim/autoload ~/.vim/bundle
+mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/colors
 
 if [ ! -f ~/.vim/autoload/pathogen.vim ]; then
     echo "Installing pathogen for vim..."
     curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+fi
+
+if [ ! -d ~/.vim/bundle/vim-material-theme ]; then
+    cd ~/.vim/bundle
+    echo "Downloading material-theme for vim..."
+    git clone https://github.com/jdkanani/vim-material-theme
+    # curl -LSso ~/.vim/colors/material-theme.vim https://raw.githubusercontent.com/jdkanani/vim-material-theme/master/colors/material-theme.vim
 fi
 
 echo "Don't forget to set your git shit"
