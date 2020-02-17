00e00774f1908cda70ec3bd32617787f59b57359
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2020-02-17 08:52:00 +0100

    Stop installing oh-my-zsh

--- new-server.sh
+++ new-server.sh
@@ -8,8 +8,3 @@ sudo apt install zsh
 
 # set zsh as the default shell
 sudo chsh -s /usr/bin/zsh "$USER"
-
-# install oh-my-zsh
-if [ ! -d ~/.oh-my-zsh ]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
