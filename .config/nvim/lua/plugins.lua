539a6580f2224f3d78c7108f9ac9420303e44623
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2022-08-23 19:42:41 +0200

    Use cargo clippy on save for rust
    Inspired by https://github.com/sharksforarms/neovim-rust/blob/master/neovim-init-lsp-cmp-rust-tools.vim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -124,17 +124,37 @@ return require("packer").startup({ function(use)
 
     use {
         "simrat39/rust-tools.nvim",
-        config = {
-            tools = {
-                inlay_hints = {
-                    auto = true,
-                    only_current_line = true,
-                    -- whether to show parameter hints with the inlay hints or not
-                    -- default: true
-                    show_parameter_hints = false,
+        config = function()
+
+            require("rust-tools").setup({
+                tools = {
+                    inlay_hints = {
+                        auto = true,
+                        show_parameter_hints = false,
+                        parameter_hints_prefix = "",
+                        other_hints_prefix = "",
+                    },
                 },
-            },
-        },
+
+                -- all the opts to send to nvim-lspconfig
+                -- these override the defaults set by rust-tools.nvim
+                -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
+                server = {
+                    -- on_attach is a callback called when the language server attachs to the buffer
+                    -- on_attach = on_attach,
+                    settings = {
+                        -- to enable rust-analyzer settings visit:
+                        -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+                        ["rust-analyzer"] = {
+                            -- enable clippy on save
+                            checkOnSave = {
+                                command = "clippy"
+                            },
+                        }
+                    }
+                },
+            })
+        end
     }
     use 'simrat39/inlay-hints.nvim'
 
