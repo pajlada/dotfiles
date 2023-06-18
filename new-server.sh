6f3487a7cfc8226c01bce5fe1e0566ea7a04136e
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2023-06-18 12:53:52 +0200

    auto create pajlada user

--- new-server.sh
+++ new-server.sh
@@ -8,3 +8,18 @@ sudo apt install zsh curl
 
 # set zsh as the default shell
 sudo chsh -s /usr/bin/zsh "$USER"
+
+# Create pajlada user
+
+if ! getent passwd pajlada >/dev/null; then
+    if [ "$USER" = "root" ]; then
+        echo "you are root xd, creating pajlada user"
+        echo "Creating pajlada user"
+        useradd \
+            --shell /usr/bin/zsh \
+            --create-home \
+            --groups sudo \
+            pajlada
+        sudo -u pajlada ssh-import-id-gh pajlada
+    fi
+fi
